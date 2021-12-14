50d5893dadbc79fce098cffd1574368e8d7ac1fd
客串一回 <cossbow@qq.com>
2021-12-14 08:38:54 +0800

    refractor

--- src/main/java/org/cossbow/Main.java
+++ src/main/java/org/cossbow/Main.java
@@ -2,12 +2,15 @@ package org.cossbow;
 
 import lombok.Data;
 import org.cossbow.dag.DAGGraph;
-import org.cossbow.dag.DAGTaskHandler;
 import org.cossbow.dag.DAGTask;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.BiFunction;
 
 public class Main {
 
@@ -35,16 +38,16 @@ public class Main {
     static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(16);
     static final Executor DeferExecutor = CompletableFuture.delayedExecutor(1, TimeUnit.SECONDS,
             EXECUTOR);
-    static final Map<NodeType, DAGTaskHandler<Node<String>, Object>> handlerMap = Map.of(
+    static final Map<NodeType, BiFunction<Node<String>, Object, CompletableFuture<Object>>> HandlerMap = Map.of(
             NodeType.CALL, new CallNodeHandler(DeferExecutor)
     );
 
     public static void main(String[] args) {
         //
-        var nodes = new ArrayList<Map.Entry<String, Node<String>>>();
+        var nodes = new HashMap<String, Node<String>>();
         for (int i = 1; i <= 6; i++) {
             var key = "N-" + i;
-            nodes.add(Map.entry(key, new Node<>(key, NodeType.CALL)));
+            nodes.put(key, new Node<>(key, NodeType.CALL));
         }
         var edges = List.of(
                 Map.entry("N-1", "N-2"),
@@ -55,15 +58,16 @@ public class Main {
                 Map.entry("N-5", "N-6"),
                 Map.entry("N-1", "N-6")
         );
-        var graph = new DAGGraph<>(nodes, edges);
+        var graph = new DAGGraph<>(nodes.keySet(), edges);
 
-        var task = new DAGTask<>(graph, n -> handlerMap.get(n.getType()));
+        var task = new DAGTask<>(graph, nodes::get, n -> HandlerMap.get(n.getType()));
         task.call().join();
 
         EXECUTOR.shutdown();
     }
 
-    public static class CallNodeHandler implements DAGTaskHandler<Node<String>, Object> {
+    public static class CallNodeHandler
+            implements BiFunction<Node<String>, Object, CompletableFuture<Object>> {
         final AtomicLong SEQ = new AtomicLong(0);
         final Executor executor;
 
@@ -72,7 +76,7 @@ public class Main {
         }
 
         @Override
-        public CompletableFuture<Object> exec(Node<String> node, Object input) {
+        public CompletableFuture<Object> apply(Node<String> node, Object input) {
             System.out.println("request: " + node.getKey());
             return CompletableFuture.supplyAsync(() -> {
                 var seq = SEQ.incrementAndGet();
--- src/main/java/org/cossbow/dag/DAGTaskHandler.java
+++
@@ -1,7 +0,0 @@
-package org.cossbow.dag;
-
-import java.util.concurrent.CompletableFuture;
-
-public interface DAGTaskHandler<N, R> {
-    CompletableFuture<R> exec(N node, R input);
-}
